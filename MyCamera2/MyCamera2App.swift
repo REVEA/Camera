c8d209f8e9e184302bb23046e5748158604bd479
REVEA <seris12600952@gmai.com>
2021-04-23 18:12:08 +0900

    Initial Commit

---
+++ MyCamera2/MyCamera2App.swift
@@ -0,0 +1,17 @@
+//
+//  MyCamera2App.swift
+//  MyCamera2
+//
+//  Created by ReDead on 2021/04/23.
+//
+
+import SwiftUI
+
+@main
+struct MyCamera2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
